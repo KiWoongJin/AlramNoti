cf2b1eecf4cb1e5fcc1b03988533e498e70ddaa0
KiWoongJin <jkw87first@naver.com>
2014-01-20 11:23:08 +0900

    AlramNoti
    실습 2.1.1

--- AlramNoti/ViewController.m
+++ AlramNoti/ViewController.m
@@ -9,10 +9,43 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIDatePicker *datePicker;
 
 @end
 
 @implementation ViewController
+- (IBAction)notiNow:(id)sender {
+    UILocalNotification *noti = [[UILocalNotification alloc]init];
+    noti.alertBody = @"노티 테스트";
+    noti.alertAction = @"확인";
+    
+    [[UIApplication sharedApplication]presentLocalNotificationNow:noti];
+}
+- (IBAction)fireNoti:(id)sender {
+    UILocalNotification *noti = [[UILocalNotification alloc]init];
+    noti.fireDate = self.datePicker.date;
+    noti.alertBody = @"지정 시간 알림";
+    noti.alertAction = @"확인";
+    noti.soundName = UILocalNotificationDefaultSoundName;
+    
+    noti.alertLaunchImage = @"image.png";
+    
+    noti.userInfo = [NSDictionary dictionaryWithObjectsAndKeys:@"object", @"key", nil];
+    [[UIApplication sharedApplication]scheduleLocalNotification:noti];
+}
+
+- (IBAction)fireNotiIn7:(id)sender {
+    UILocalNotification *noti = [[UILocalNotification alloc]init];
+    noti.fireDate = [NSDate dateWithTimeIntervalSinceNow:7];
+    noti.alertBody = @"7 seconds";
+    noti.alertAction = @"확인";
+    noti.soundName = UILocalNotificationDefaultSoundName;
+    
+    noti.userInfo = nil;
+    
+    [[UIApplication sharedApplication]scheduleLocalNotification:noti];
+}
+
 
 - (void)viewDidLoad
 {
